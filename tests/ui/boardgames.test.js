005ed0c204007ad0bee8a5487b948a28b1ae916f
hmustafa95 <hmustafa1509@gmail.com>
2023-12-11 14:24:19 +0200

    changed url on ui tests

--- tests/ui/boardgames.test.js
+++ tests/ui/boardgames.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check boardgames page', async ({ page }) => {
-    await page.goto('http://localhost:8080/boardgames');
+    await page.goto('https://exam-test-2.onrender.com/boardgames');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
